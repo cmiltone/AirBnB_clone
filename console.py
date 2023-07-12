af0f4b34affcbc6a58debf8efa2dba5f215c866b
Castro Hamiltone <castromiltone@gmail.com>
2023-07-13 01:13:38 +0300

    adds show command to console

--- console.py
+++ console.py
@@ -6,7 +6,7 @@ import cmd
 
 # custom packages/modules
 from models.base_model import BaseModel
-
+from models import storage
 
 class HBNBCommand(cmd.Cmd):
     """a class for the console"""
@@ -43,8 +43,31 @@ class HBNBCommand(cmd.Cmd):
         elif args[0] == "BaseModel":
             model = BaseModel()
             model.save()
+            print(model.id)
         else:
             print("** class doesn't exist **")
+
+    def do_show(self, a):
+        """prints string representation of instance"""
+        classes = ["BaseModel"]
+        args = a.split()
+
+        if args[0] == "":
+            print("** class name missing **")
+        elif args[0] not in classes:
+            print("** class doesn't exist ** ")
+        elif len(args) == 1 or args[1] == '':
+            print("** instance id missing **")
+        else:
+            objs = storage.all()
+            id = "{}.{}".format(args[0], args[1])
+            obj = objs.get(id)
+            if obj is None:
+                print("* no instance found **")
+            else:
+                print(obj)
+                
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
