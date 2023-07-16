ec7458138817a3429a2587f8cfe60539e00ad3db
cmiltone <castromiltone@gmail.com>
2023-07-16 20:54:26 +0300

    file storage tests

---
+++ tests/test_file_storage.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Unittest for storage
+"""
+
+
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """Test Cases for Base class"""
+
+    def test_all(self):
+        """Test to_dict method correctness"""
+        storage = FileStorage()
+        pass
+        # test it
